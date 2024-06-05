1499e7d83698d9a929480dcd042fa7304e2545d8
Malcolm Nixon <malcolm.nixon@elbitsystems-us.com>
2024-06-05 18:05:53 -0400

    Add IAR EWARM workflows

---
+++ test/DemaConsulting.SpdxWorkflows.Tests/AddIarEwArmPackage.cs
@@ -0,0 +1,27 @@
+﻿namespace DemaConsulting.SpdxWorkflows.Tests;
+
+[TestClass]
+public class AddIarEwArmPackage : AddPackageTest
+{
+    [TestMethod, TestCategory("AnyOS")]
+    public void TestAddIarEwArmPackage()
+    {
+        var doc = RunAddPackageWorkflow(
+            "AddIarEwArmPackage.yaml",
+            "spdx=spdx.json",
+            "id=SPDXRef-Package-IAR-EWARM-9.40.2.374",
+            "version=9.40.2.374");
+
+        // Verify the package was added
+        var package = Array.Find(doc.Packages, p => p.Id == "SPDXRef-Package-IAR-EWARM-9.40.2.374");
+        Assert.IsNotNull(package);
+
+        // Verify the information
+        Assert.AreEqual("IAR Embedded Workbench for Arm", package.Name);
+        Assert.AreEqual("9.40.2.374", package.Version);
+        Assert.AreEqual("Organization: IAR Systems AB", package.Supplier);
+        Assert.AreEqual("Organization: IAR Systems AB", package.Originator);
+        Assert.AreEqual("https://www.iar.com/products/architectures/arm/iar-embedded-workbench-for-arm", package.HomePage);
+        Assert.AreEqual("Complete development environment for Arm", package.Summary);
+    }
+}
