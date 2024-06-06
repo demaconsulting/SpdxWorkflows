2112d7ee05c36fa47335a00b7104b1cbf9e2ff95
Malcolm Nixon <Malcolm.nixon@gmail.com>
2024-06-05 21:46:34 -0400

    Added msbuild workflows. (#5)
    Added use of DemaConsulting.VSWhereTool to find MSBuild.
    Added PURLs for some packages.
    
    Co-authored-by: Malcolm Nixon <malcolm.nixon@elbitsystems-us.com>

---
+++ test/DemaConsulting.SpdxWorkflows.Tests/GetMsBuildVersion.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+
+namespace DemaConsulting.SpdxWorkflows.Tests;
+
+[TestClass]
+public class GetMsBuildVersion : WorkflowTest
+{
+    [TestMethod, TestCategory("Windows")]
+    public void TestGetMsBuildVersion()
+    {
+        // Use vswhere to find MSBuild
+        Runner.Run(
+            out var msbuild,
+            "dotnet",
+            "vswhere",
+            "-latest",
+            "-requires",
+            "Microsoft.Component.MSBuild",
+            "-find",
+            @"MSBuild\**\Bin\MSBuild.exe");
+
+        // Clean up the path
+        msbuild = msbuild.Trim();
+
+        // Run the workflow
+        var exitCode = RunWorkflow(
+            out var output,
+            "GetMsBuildVersion.yaml",
+            $"msbuild={msbuild}",
+            "--verbose");
+
+        // Verify we found a valid MSBuild version
+        Assert.AreEqual(0, exitCode);
+        Assert.IsTrue(Regex.IsMatch(output, @"version = \d+\.\d+\.\d+"));
+    }
+}
