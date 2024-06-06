ac214a5fdac7857bc30e52138cc94013d2c8e516
Malcolm Nixon <malcolm.nixon@elbitsystems-us.com>
2024-06-05 21:42:32 -0400

    Added msbuild workflows. Added use of DemaConsulting.VSWhereTool to find MSBuild. Added PURLs for some packages.

---
+++ test/DemaConsulting.SpdxWorkflows.Tests/AddGccPackage.cs
@@ -0,0 +1,36 @@
+﻿namespace DemaConsulting.SpdxWorkflows.Tests;
+
+[TestClass]
+public class AddGccPackage : AddPackageTest
+{
+    [TestMethod, TestCategory("AnyOS")]
+    public void TestAddGccPackage()
+    {
+        var doc = RunAddPackageWorkflow(
+            "AddGccPackage.yaml",
+            "spdx=spdx.json",
+            "id=SPDXRef-Package-GCC-11.4.0",
+            "version=11.4.0");
+
+        // Verify the package was added
+        var package = Array.Find(doc.Packages, p => p.Id == "SPDXRef-Package-GCC-11.4.0");
+        Assert.IsNotNull(package);
+
+        // Verify the information
+        Assert.AreEqual("GNU Compiler Collection", package.Name);
+        Assert.AreEqual("GPL-3.0-or-later", package.ConcludedLicense);
+        Assert.AreEqual("Copyright (C) 2009 Free Software Foundation, Inc. <http://fsf.org/>", package.CopyrightText);
+        Assert.AreEqual("11.4.0", package.Version);
+        Assert.AreEqual("Organization: GNU", package.Originator);
+        Assert.AreEqual("https://gcc.gnu.org/", package.HomePage);
+        Assert.AreEqual(
+            "The GNU Compiler Collection includes front ends for C, C++, Objective-C, Fortran, Java, and Ada, as well as libraries for these languages (libstdc++, libgcj,...).",
+            package.Summary);
+
+        // Verify the PURL
+        Assert.AreEqual(1, package.ExternalReferences.Length);
+        Assert.AreEqual(SpdxModel.SpdxReferenceCategory.PackageManager, package.ExternalReferences[0].Category);
+        Assert.AreEqual("purl", package.ExternalReferences[0].Type);
+        Assert.AreEqual("pkg:github/gcc-mirror/gcc@releases/gcc-11.4.0", package.ExternalReferences[0].Locator);
+    }
+}
---
+++ test/DemaConsulting.SpdxWorkflows.Tests/AddMsBuildPackage.cs
@@ -0,0 +1,35 @@
+﻿namespace DemaConsulting.SpdxWorkflows.Tests;
+
+[TestClass]
+public class AddMsBuildPackage : AddPackageTest
+{
+    [TestMethod, TestCategory("AnyOS")]
+    public void TestAddMsBuildPackage()
+    {
+        var doc = RunAddPackageWorkflow(
+            "AddMsBuildPackage.yaml",
+            "spdx=spdx.json",
+            "id=SPDXRef-Package-MSBuild-17.10.4",
+            "version=17.10.4");
+
+        // Verify the package was added
+        var package = Array.Find(doc.Packages, p => p.Id == "SPDXRef-Package-MSBuild-17.10.4");
+        Assert.IsNotNull(package);
+
+        // Verify the information
+        Assert.AreEqual("Microsoft Build Engine", package.Name);
+        Assert.AreEqual("MIT", package.ConcludedLicense);
+        Assert.AreEqual("Copyright (c) .NET Foundation and Contributors", package.CopyrightText);
+        Assert.AreEqual("17.10.4", package.Version);
+        Assert.AreEqual("Organization: Microsoft Corporation", package.Supplier);
+        Assert.AreEqual("Organization: Microsoft Corporation", package.Originator);
+        Assert.AreEqual("https://learn.microsoft.com/en-us/visualstudio/msbuild/msbuild", package.HomePage);
+        Assert.AreEqual("The Microsoft Build Engine is a platform for building applications.", package.Summary);
+
+        // Verify the PURL
+        Assert.AreEqual(1, package.ExternalReferences.Length);
+        Assert.AreEqual(SpdxModel.SpdxReferenceCategory.PackageManager, package.ExternalReferences[0].Category);
+        Assert.AreEqual("purl", package.ExternalReferences[0].Type);
+        Assert.AreEqual("pkg:github/dotnet/msbuild@v17.10.4", package.ExternalReferences[0].Locator);
+    }
+}
